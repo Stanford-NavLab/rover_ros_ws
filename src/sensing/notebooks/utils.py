eb9b9c30eae2495a276bb9fe8ad41f01e4cc02e1
Adam Dai <adamdai97@gmail.com>
2022-05-17 17:14:14 -0700

    detect landmark

--- src/sensing/notebooks/utils.py
+++ src/sensing/notebooks/utils.py
@@ -58,6 +58,57 @@ def bounds_mask(P_2D, width=0.5):
     return mask
 
 
+def rotate_points(P, theta):
+    """Rotate 2D points by theta
+
+    Parameters
+    ----------
+    P : np.array (n_pts x 2)
+        2D points
+    theta : float
+        Angle to rotate in radians
+    
+    """
+    R = np.array([[np.cos(theta), -np.sin(theta)],
+                  [np.sin(theta), np.cos(theta)]])
+    return (R @ P.T).T
+
+
+def detect_landmark(P, x_hat, d_thresh=6.0, lidar_height=0.15):
+    """Detect landmark
+
+    Given point cloud and state estimate, detect landmark position in robot local frame.
+    
+    Parameters
+    ----------
+    P : np.array (n_pts x 3)
+        3D point cloud
+    x_hat : np.array (4 x 1)
+        State estimate (x, y, theta, v)
+    
+    Returns
+    -------
+    np.array (3)
+        Estimated 3D landmark position in local frame
+    
+    """
+    # Distance filter  NOTE: may not be needed anymore
+    P = dist_filter(P, d_thresh)
+    # Ground removal
+    P = P[P[:,2] > -lidar_height,:] 
+    # Transform to global frame in 2D
+    P_2D = P[:,:2]
+    P_2D_global = rotate_points(P_2D, x_hat[2])
+    P_2D_global = P_2D_global + x_hat[:2]
+    # Use search region to extract landmark in global frame
+    mask = np.ones(len(P), dtype=bool)
+    mask = mask & ((P_2D_global[:,0] <= LM_BOX_XMAX) & (P_2D_global[:,0] >= LM_BOX_XMIN))
+    mask = mask & ((P_2D_global[:,1] <= LM_BOX_YMAX) & (P_2D_global[:,1] >= LM_BOX_YMIN))
+
+    landmark_pts = P[mask]
+    return np.mean(landmark_pts, axis=0)
+
+
 def extract_landmarks(P, d_thresh=6.0, lidar_height=0.15, dbscan_eps=0.2, dbscan_minpts=10):
     """Extract landmark positions from point cloud
 
